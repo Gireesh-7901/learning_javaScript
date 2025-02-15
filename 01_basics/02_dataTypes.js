7ee3cc44bc5d20a3a2806c1dfe0446a6ee3d9232
Gireesh-7901 <gireeshnimmala7@gmail.com>
2025-02-15 12:26:38 +0000

    Learned about data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict" // To represent the JS code written in this doc uses JS standards
+
+//alert(2+2) // we are using nodejs not browser
+
+//code readability
+
+let name = "Gireesh" //string
+let age = 23 // number
+let isLoggedIn = false // boolean
+
+//data types
+// number => max range (2 to power of 53)
+// bigInt => we need to use if the number range is greater than number
+// string => ""
+// boolean => true/false
+// null => It is a standalone value
+// undefined
+// symbol => unique
+
+//object
+
+console.log(typeof null); //null is an object
+console.log(typeof undefined);
