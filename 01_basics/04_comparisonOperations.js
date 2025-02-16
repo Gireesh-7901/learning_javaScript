b16963e5373145a39c8a835caf1c6b27753bbce1
Gireesh-7901 <gireeshnimmala7@gmail.com>
2025-02-16 10:05:33 +0000

    Learned about comparisionOperators

---
+++ 01_basics/04_comparisonOperations.js
@@ -0,0 +1,26 @@
+"use strict"
+
+// console.log("2" == 2);
+// console.log("02" == 2);
+// console.log("2a" == 2);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+//console.log("1" == true);
+
+//strict check (===,!===)
+
+console.log("2" === 2);
+
+
+
+/*
+    In normal comparision operators(>,<,>=,<=,==) , implicit data converion will happen then checks the data
+    But in case of strict comparision operator(>==,<==,===), first checks the dataType then moves to data check
+*/
