c04766e064ed9b98c8c4161cd82f51ed14abc2a7
Gireesh-7901 <gireeshnimmala7@gmail.com>
2025-02-17 04:41:04 +0000

    Learned about Number and Maths classes

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,31 @@
+
+const score = 400;
+
+//console.log(score);
+const balance = new Number(100);
+// console.log(balance);
+// console.log(balance.toFixed(2)); // returns string
+// console.log(balance.toPrecision(2)); // returns string
+//console.log(balance.toString());
+const oneCrore = 10000000;
+//console.log(oneCrore.toLocaleString('en-IN')); //returns string
+
+
+//----------Math-------------
+
+//console.log(Math.abs(-2)); // returns +'ve number 
+//console.log(Math.round(3.5));
+// console.log(Math.ceil(2.1));
+// console.log(Math.floor(2.9));
+//console.log(Math.sqrt(2));
+// console.log(Math.min(9,4,1,5));
+// console.log(Math.max(9,4,1,5));
+
+//console.log(Math.random()); returns a number between 0 and 1 inclusive
+console.log(Math.random() * 10);
+console.log(Math.floor((Math.random()*10 ) + 1));
+
+const min = 10;
+const max = 20;
+
+console.log((Math.random() * (max - min)) + 1 + min);
